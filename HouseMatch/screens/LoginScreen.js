6d87c4f1c302edb137798bef0d70ac1bd2ec7405
SatjeevanRiar <43180940+SatjeevanRiar@users.noreply.github.com>
2022-02-14 23:46:06 -0500

    Finally

---
+++ HouseMatch/screens/LoginScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Login Screen</Text>
+    </View>
+  )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({})
